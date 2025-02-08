5f40121af86383c42c040f8dc7b89b6e4173ae92
MissShot7 <pavelhob@outlook.com>
2025-02-08 10:25:57 +0100

    prototype_2
    Now can run in system tray

--- Remote-Shutdown/MainWindow.xaml.cs
+++ Remote-Shutdown/MainWindow.xaml.cs
@@ -9,6 +9,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.IO;
+using System.ComponentModel;
+using System.Windows.Forms;
+using Application = System.Windows.Application;
+
 
 namespace Remote_Shutdown
 {
@@ -19,6 +23,8 @@ namespace Remote_Shutdown
     {
         public static MainWindow Instance { get; private set; }
         static string LogFilePath;
+
+        
         public MainWindow()
         {
             InitializeComponent();
@@ -40,6 +46,9 @@ namespace Remote_Shutdown
                 string content = File.ReadAllText(LogFilePath);
                 NCL(content, false);
             }
+            CreateNotifyIcon(); //system tray
+            this.Hide(); //starts hidden
+            ServerToggle(); //start server
         }
         public static void NCL(string _txt, bool AddToLogFile=true) //New console log
         {
@@ -53,21 +62,124 @@ namespace Remote_Shutdown
             });
         }
 
-
-        private void OnOffButton_Click(object sender, RoutedEventArgs e)
+        private void ServerToggle()
         {
             if (OnOffButton.Content.ToString() == "On") //power of
             {
                 OnOffButton.Content = "Off";
                 ServerClass.StopServer();
-            } else //power on
+            }
+            else //power on
             {
                 if (ServerClass.StartServer()) //if success
                 {
                     OnOffButton.Content = "On";
                 }
             }
+        }
+        private void OnOffButton_Click(object sender, RoutedEventArgs e)
+        {
+            ServerToggle();
 
         }
+
+        //tray
+        private System.Windows.Forms.NotifyIcon m_notifyIcon;
+        private WindowState m_storedWindowState = WindowState.Normal;
+
+        private void CreateNotifyIcon()
+        {
+            // Initialize NotifyIcon
+            m_notifyIcon = new System.Windows.Forms.NotifyIcon();
+            m_notifyIcon.BalloonTipText = "Remote Shutdown has been minimised. Click the tray icon to show.";
+            m_notifyIcon.BalloonTipTitle = "Remote Shutdown";
+            m_notifyIcon.Text = "Remote Shutdown";
+            m_notifyIcon.Icon = new System.Drawing.Icon("trayIcon.ico");
+
+            // Create a context menu for the tray icon
+            var contextMenu = new System.Windows.Forms.ContextMenuStrip();
+            var openItem = new System.Windows.Forms.ToolStripMenuItem("Open", null, OpenItem_Click);
+            var closeItem = new System.Windows.Forms.ToolStripMenuItem("Close", null, CloseItem_Click);
+            contextMenu.Items.Add(openItem);
+            contextMenu.Items.Add(closeItem);
+            m_notifyIcon.ContextMenuStrip = contextMenu;
+            
+
+            m_notifyIcon.Click += new EventHandler(m_notifyIcon_Click);
+            m_notifyIcon.Visible = true; // Make sure the icon is visible
+        }
+
+        private void OpenItem_Click(object sender, EventArgs e)
+        {
+            // Restore the app when 'Open' is clicked
+            Show();
+            WindowState = m_storedWindowState;
+        }
+
+        private void CloseItem_Click(object sender, EventArgs e)
+        {
+            // Close the app when 'Close' is clicked from the tray menu
+            CloseApp();
+        }
+
+        private void m_notifyIcon_Click(object sender, EventArgs e)
+        {
+            // Check for left mouse button click
+            if (e is System.Windows.Forms.MouseEventArgs mouseEventArgs && mouseEventArgs.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                // Show the app when the tray icon is clicked with the left mouse button
+                Show();
+                WindowState = m_storedWindowState;
+            }
+        }
+
+        private void CloseApp()
+        {
+            // Close the app
+            m_notifyIcon.Dispose();
+            m_notifyIcon = null;
+            Application.Current.Shutdown();
+            ServerClass.StopServer();
+        }
+
+        // Override OnClosing event to prevent closing the app, hide instead
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+            if (e.Cancel == false) // Allow the user to close the app by choosing "Close" from the context menu
+            {
+                e.Cancel = true; // Prevent the app from closing
+                Hide(); // Hide the window instead
+            }
+        }
+
+        void OnStateChanged(object sender, EventArgs args)
+        {
+            if (WindowState == WindowState.Minimized)
+            {
+                Hide(); // Hide the window when minimized
+            }
+            else
+            {
+                m_storedWindowState = WindowState;
+            }
+        }
+
+        void OnIsVisibleChanged(object sender, DependencyPropertyChangedEventArgs args)
+        {
+            CheckTrayIcon();
+        }
+
+        void CheckTrayIcon()
+        {
+            ShowTrayIcon(!IsVisible);
+        }
+
+        void ShowTrayIcon(bool show)
+        {
+            if (m_notifyIcon != null)
+                m_notifyIcon.Visible = show;
+        }
+
     }
 }
